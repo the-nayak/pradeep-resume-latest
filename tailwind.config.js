c35e5106a08128c751a57f0ef0bf0d81b3f0e6eb
Pradeep Nayak <nayak54325@gmail.com>
2022-06-04 14:24:40 +0530

    page ready

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    fontFamily: {
+      primary: ["Outfit", "sans-serif"],
+      secondary: ["DM Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
